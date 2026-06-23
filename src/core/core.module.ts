3059ad7bbeae30a4db6ce68e43113e8d5f654818
Pavel-Baranenko <baranenko_work@mail.ru>
2026-06-23 19:17:55 +0300

    graphql config

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,11 +1,20 @@
+import { ApolloDriver } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util'
+import { getGraphqlConfig } from './config/graphql.config'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ ignoreEnvFile: !IS_DEV_ENV, isGlobal: true }),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			imports: [ConfigModule],
+			useFactory: getGraphqlConfig,
+			inject: [ConfigService]
+		}),
 		PrismaModule
 	]
 })
