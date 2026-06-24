fdb25e1e0c4f35a947a78bddbdc0c2be91c4ce8b
Pavel-Baranenko <baranenko_work@mail.ru>
2026-06-24 16:27:44 +0300

    redis session store

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import * as cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
 import { CoreModule } from './core/core.module'
+import { ms, type StringValue } from './shared/utils/ms.util'
+import { parseBoolean } from './shared/utils/parse-boolean.util'
 
 async function bootstrap() {
 	const app = await NestFactory.create(CoreModule)
@@ -10,6 +13,23 @@ async function bootstrap() {
 
 	app.use(cookieParser(config.getOrThrow<string>('COOKIE_SECRET')))
 
+	const sessionConfig = {
+		secret: config.getOrThrow<string>('SESSION_SECRET'),
+		name: config.getOrThrow<string>('SESSION_NAME'),
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+			maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+			httpOnly: parseBoolean(
+				config.getOrThrow<string>('SESSION_HTTP_ONLY')
+			),
+			secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+			sameSite: 'lax' as const
+		}
+	}
+
+	app.use(session(sessionConfig))
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true
